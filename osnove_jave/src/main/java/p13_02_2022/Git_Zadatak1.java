5cdc62946e13fcedb6e09fcb27d747bf37a386af
Slobodan Kostic <stickopn@gmail.com>
2026-01-29 21:42:33 +0100

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/p13_02_2022/Git_Zadatak1.java
@@ -0,0 +1,11 @@
+package p13_02_2022;
+
+public class Git_Zadatak1 {
+    static void main() {
+        System.out.println("git clone");
+        System.out.println("git status");
+        System.out.println("git add putanja do fajla");
+        System.out.println("git commit -m poruka");
+        System.out.println("git push");
+    }
+}
